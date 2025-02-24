10a9d3249128ae1e18006d924fa9ca63106e5fa9
Lourdes Jiménez <00090220@uca.edu.sv>
2025-02-23 20:28:16 -0600

    Add tarea

--- ejercicio.cpp
+++ ejercicio.cpp
@@ -3,9 +3,22 @@
 using namespace std;
 
 void imprimirTriangulo(int altura) {
+    for (int i = 1; i <= altura; i++)
+    {
+        //Mientras el valor de j sea menos que i, va a imprimir j-veces el *, simboliza columnas
+        for (int j = 0; j < i; j++)
+        {
+            cout << "*";
+        }
+        //Salto de linea
+        cout << "\n";
+    }
 }
 
 int main() {
-
+    int n1, n2, n3;
+    imprimirTriangulo(n1);
+    imprimirTriangulo(n2);
+    imprimirTriangulo(n3);
     return 0;
 }
